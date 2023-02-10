519c0a8314753f7943c2819c1ef6c02b14d95015
Ирина <irina.kanus3@gmail.com>
2023-02-10 20:46:37 +0400

    done

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,31 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        int leap;
+        if ((year < 0) || (month < 1)|| (month > 12)) {
+            System.out.println("invalid date");
+        } else {
+            if (year % 400 == 0) {
+                leap = 1;
+            } else if (year % 100 == 0) {
+                leap = 0;
+            } else if (year % 4 == 0) {
+                leap = 1;
+            } else leap = 0;
+
+            switch (month) {
+                case 1, 3, 5, 7, 8, 10, 12:
+                    System.out.println(31);
+                    break;
+                case 4, 6, 9, 11:
+                    System.out.println(30);
+                    break;
+                case 2:
+                    if (leap == 1) {
+                        System.out.println(29);
+                    } else System.out.println(28);
+            }
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/TriangleSidesValidator.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class TriangleSidesValidator {
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        int res = 0;
+        if ((firstSide > 0) && (secondSide > 0) && (thirdSide > 0)) {
+        if ((firstSide + secondSide > thirdSide) && (secondSide +thirdSide > firstSide) && (firstSide + thirdSide > secondSide)){
+            res = 1;
+        }
+}
+        switch (res){
+            case 1:
+                System.out.println("this is a valid triangle");
+                break;
+            case 0:
+                System.out.println("it's not a triangle");
+        }
 
     }
 }
